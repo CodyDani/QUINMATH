f428bb42ab0ec7c9b00afdd33b5e4304a6a8df75
Daniel Agada Alfred <danielagadaalfred@gmail.com>
2025-07-28 15:56:38 +0100

    Update result.js

--- JS/result.js
+++ JS/result.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const quizScore = localStorage.getItem('quizScore');
 
     retry.addEventListener('click', () => {
-        window.location.href = '/QUINMATH/game-page.html';
+        window.location.href = '../game-page.html';
     })
 
     endGameTwo.addEventListener('click', () => {
-        window.location.href = '/QUINMATH/index.html';
+        window.location.href = '../index.html';
     })
 
     userScore.textContent = quizScore;
